d6c2e4611c774c030b63381b80158311936f021d
Reiza Haniff <reiza.haniff@dec.uwi.edu>
2019-03-26 18:51:33 -0400

    fancy scrollbar

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019032616;
+$plugin->version   = 2019032617;
 $plugin->requires  = 2018112800;
 $plugin->component = 'theme_minusone';
