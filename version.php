06f3ac8c2d185b301bd8ee6f9f3921992da9318c
Reiza Haniff <reiza.haniff@dec.uwi.edu>
2019-03-26 19:13:31 -0400

    campus specific content headings

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019032617;
+$plugin->version   = 2019032618;
 $plugin->requires  = 2018112800;
 $plugin->component = 'theme_minusone';
