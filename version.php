7df273372b809212ca1955f5f261bc389cb794df
Reiza Haniff <reiza.haniff@dec.uwi.edu>
2019-03-27 06:10:48 -0400

    cave hill related changes
    - cave hill related changes for better readability
    - footer clean up

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019032700;
+$plugin->version   = 2019032701;
 $plugin->requires  = 2018112800;
 $plugin->component = 'theme_minusone';
