e0d1416cc71c5879d78666fcd51545093248f909
Reiza Haniff <reiza.haniff@dec.uwi.edu>
2019-03-27 22:02:53 -0400

    version bump

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019032706;
+$plugin->version   = 2019032707;
 $plugin->requires  = 2018112800;
 $plugin->component = 'theme_minusone';
